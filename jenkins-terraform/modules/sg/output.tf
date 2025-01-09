8a3992d01bb0bfe244653c5ae9f587de00a7939f
nikitaram882799 <90086505+nikitaram882799@users.noreply.github.com>
2025-01-09 12:45:24 +0530

    Update output.tf

--- jenkins-terraform/modules/sg/output.tf
+++ jenkins-terraform/modules/sg/output.tf
@@ -6,10 +6,3 @@ output "dev_sg" {
   value = aws_security_group.dev_sg.id
 }
 
-output "rds_mysql_sg_id" {
-  value = aws_security_group.rds_mysql_sg.id
-}
-
-output "python_api_sg" {
-  value = aws_security_group.python_api_sg.id
-}
